9e9019b6691cdecc2ded52c46741d7acca1575ad
soparifly <soparifly@naver.com>
2021-10-07 11:38:11 +0900

    loginV1

---
+++ react-login-v1/src/comps/Logout.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useUserContext } from "../context/UserContextProvider";
+import { useHistory } from "react-router-dom";
+
+const Logout = () => {
+  const { setAccount } = useUserContext();
+  const history = useHistory();
+
+  useEffect(() => {
+    const logout = async () => {
+      const res = await fetch("http://localhost:8080/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+        credentials: "include",
+      });
+      await setAccount([]);
+      history.replace("/");
+    };
+    logout();
+  }, []);
+  return <div></div>;
+};
+
+export default Logout;
